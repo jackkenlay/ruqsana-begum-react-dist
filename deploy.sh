d328a35f8a7acaf6e0f01fa161321d336b0ac8b6
Jack Kenlay <jack.kenlay@reachplc.com>
2020-02-28 22:55:14 +0000

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 echo "deploying"
 
+
+
 cp -R /Users/jackkenlay/personal/ruqsana-begum-react/build/. /Users/jackkenlay/personal/ruqsana-begum-react-dist/
 
 git --git-dir="/Users/jackkenlay/personal/ruqsana-begum-react-dist/.git" status
 
 git --git-dir="/Users/jackkenlay/personal/ruqsana-begum-react-dist/.git" add -A
+git --git-dir="/Users/jackkenlay/personal/ruqsana-begum-react-dist/.git" status
 
 git --git-dir="/Users/jackkenlay/personal/ruqsana-begum-react-dist/.git" commit -m "Deploy"
+git --git-dir="/Users/jackkenlay/personal/ruqsana-begum-react-dist/.git" status
 
 git --git-dir="/Users/jackkenlay/personal/ruqsana-begum-react-dist/.git" push
